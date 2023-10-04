567baef8d41e4498f76af868181944843eeb1327
JhulyVivas <131303835+JhulyVivas@users.noreply.github.com>
2023-10-04 15:41:08 -0500

    challenge 09

--- challenge09/src/Components/gifGrid/GifGrid.jsx
+++ challenge09/src/Components/gifGrid/GifGrid.jsx
@@ -1,31 +1,21 @@
 import React from "react";
-
-const  getGifs = async ( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=igZyIiG54TEWM8952p1gfFrkWrklgELE&q=category&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
-    const resp = await fetch(url)
-    const { data } = await resp.json()
-    const gifs = data.map( img => {
-        return {
-            id: img.id,
-            title: img.title,
-            url: img.images.fixed_height.url
-        }
-    })
-
-    return gifs
-}
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+import { GifItem } from "./GifItem";
+import "./gifGrid.css"
 
 export const GifGrid = ({category}) => {
-    getGifs(category)
+    const {images, isLoading} = useFetchGifs(category)
 
     return (
-        <div>
-            <h3>
-                {category}
-            </h3>
-            <p>
-                Hello World
-            </p>
-        </div>
-  )
+        <>
+            <h3> { category } </h3>
+            <div className="card-grid">
+                {
+                    images.map( (image, key) => {
+                        return <GifItem key={key} {...image}></GifItem>
+                    })
+                }
+            </div>
+        </>
+    )
 }
