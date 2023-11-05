fc7c92b0329406b2f5e68d39af4ceca7e0913f39
JhulyVivas <131303835+JhulyVivas@users.noreply.github.com>
2023-11-04 23:58:31 -0500

    Challenge 16 completo

---
+++ challenge16/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavComponent } from '../components/NavComponent'
+
+export const HomePage = () => {
+    return (
+        <>
+        <NavComponent />
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        </>
+    )
+}
